526b3b4a372b2df19888883f79c9bd6ecd3d3220
Michael Ashenafi <ashenafimichael@gmail.com>
2022-11-18 05:53:57 -0600

    added Eureka server instance

--- EurekaServer/src/main/java/miu/edu/eurekaserver/EurekaServerApplication.java
+++ EurekaServer/src/main/java/miu/edu/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package miu.edu.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
